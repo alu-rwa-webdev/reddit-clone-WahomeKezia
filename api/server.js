890b1af19daab36946e9dea8f29acbca78169d29
KeziaWahome <k.wahome@alustudent.com>
2022-05-12 20:08:08 +0200

    api commits

--- api/server.js
+++ api/server.js
@@ -1,75 +1,43 @@
-import  express from "express"
+import express from 'express';
 import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
 import mongoose from "mongoose";
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import User from "./models/User.js";
+import Comment from "./models/Comment.js";
+import VotingRoutes from "./VotingRoutes.js";
 
-
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
+const secret = 'secret123';
+const app = express();
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }));
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
-// // connecting to the DataBase
-//  await mongoose.connect('mongodb://127.0.0.1:27017/', {useNewUrlParser:true,useUnifiedTopology:true,});
+app.use(VotingRoutes);
 
+function getUserFromToken(token) {
+  const userInfo = jwt.verify(token, secret);
+  return User.findById(userInfo.id);
+}
+
+// await mongoose.connect('mongodb://localhost:27017/reddit', {useNewUrlParser:true,useUnifiedTopology:true,});
 // const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
+// db.on('error', console.log);
+
+// app.get('/', (req, res) => {
+//   res.send('ok');
 // });
 
-// const username = "kezia";
-// const password = "MyNewPass";
-// const cluster = "RedditCloneDB";
-// const dbname = "myFirstDatabase";
-
-// mongoose.connect(
-//    "mongodb+srv://kezia:MyNewPass@redditclonedb.4etyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-//   {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   }
-// );
-
-// const username = "kezia";
-// const password = "MyNewPass";
-// const cluster = "redditclonedb";
-// const dbname = "redditDB";
-
-// await mongoose.connect(
-//   `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
-//   {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true
-//   }
-// );
-
-
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
-
-
-app.get('/', (req, res) => {
-    res.send('ok');
-  });
-
-  //API end points for register 
-  app.post('/register', (req, res) => {
-    const {email,username} = req.body;
-    const password = bcrypt.hashSync(req.body.password, 10);
-    const user = new User({email,username,password});
+app.post('/register', (req, res) => {
+  const {email,username} = req.body;
+  const password = bcrypt.hashSync(req.body.password, 10);
+  const user = new User({email,username,password});
   user.save().then(user => {
     jwt.sign({id:user._id}, secret, (err, token) => {
       if (err) {
@@ -85,9 +53,91 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/user', (req, res) => {
+  const token = req.cookies.token;
+
+  getUserFromToken(token)
+    .then(user => {
+      res.json({username:user.username});
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+
+});
+
+app.post('/login', (req, res) => {
+  const {username, password} = req.body;
+  User.findOne({username}).then(user => {
+    if (user && user.username) {
+      const passOk = bcrypt.compareSync(password, user.password);
+      if (passOk) {
+        jwt.sign({id:user._id}, secret, (err, token) => {
+          res.cookie('token', token).send();
+        });
+      } else {
+        res.status(422).json('Invalid username or password');
+      }
+    } else {
+      res.status(422).json('Invalid username or password');
+    }
+  });
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').send();
+});
+
+app.get('/comments', (req, res) => {
+  const search = req.query.search;
+  const filters = search
+    ? {body: {$regex: '.*'+search+'.*'}}
+    : {rootId:null};
+  Comment.find(filters).sort({postedAt: -1}).then(comments => {
+    res.json(comments);
+  });
+});
+
+app.get('/comments/root/:rootId', (req, res) => {
+  Comment.find({rootId:req.params.rootId}).sort({postedAt: -1}).then(comments => {
+    res.json(comments);
+  });
+});
+
+app.get('/comments/:id', (req, res) => {
+  Comment.findById(req.params.id).then(comment => {
+    res.json(comment);
+  });
+});
+
+app.post('/comments', (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
+  getUserFromToken(token)
+    .then(userInfo => {
+      const {title,body,parentId,rootId} = req.body;
+      const comment = new Comment({
+        title,
+        body,
+        author:userInfo.username,
+        postedAt:new Date(),
+        parentId,
+        rootId,
+      });
+      comment.save().then(savedComment => {
+        res.json(savedComment);
+      }).catch(console.log);
+    })
+    .catch(() => {
+      res.sendStatus(401);
+    });
+});
 
 app.listen(4000 , () => {
     console.log("Server is running at port 4000");
   });
 
-  
