b154139d2daa1e640584df24e9dfbd587e5e7c07
KeziaWahome <k.wahome@alustudent.com>
2024-01-04 03:51:14 +0200

    updating for heroku deployment

--- api/server.js
+++ api/server.js
@@ -335,6 +335,20 @@ app.post('/comments', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+// -------------deployment to heroku---------------
+__dirname = path.resolve();
+if (process.env.NODE_ENV ==="production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+  
+}else{
+  app.get("/",(req,res) => {
+    res.send("API is running...");
+  });
+}
 
 });
 
